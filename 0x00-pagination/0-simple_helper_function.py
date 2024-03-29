123a277ce763a892897f96453cd5fadb956ebb38
GArdennes <kevin.afachao@gmail.com>
2024-03-29 10:03:16 -0700

    Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -19,10 +19,10 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
           - start index
           - end index
     """
-    if page < 1:
-        raise ValueError(
-                "Page number must be greater than or equal to 1"
-                )
+    # if page < 1:
+    #    raise ValueError(
+    #            "Page number must be greater than or equal to 1"
+    #            )
     start = (page - 1) * page_size
     end = start + page_size
     return (start_index, end_index)
