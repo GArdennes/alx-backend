25b077554fd7c4bcc1a7ecc7d671a5fe4473a3ea
GArdennes <kevin.afachao@gmail.com>
2024-04-01 07:56:01 -0700

    Task 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
@@ -40,4 +40,44 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+            """
+            Methodd that returns a dictionary with hyper pagination info
+            Args:
+              index: the starting index for the page
+              page_size: the number of items per page
+
+            Returns:
+              A dictionary containing:
+               - index: The current start index of the page
+               - next_index: The next index to query with
+               - page_size: The current page size
+               - data: The actual page of the dataset
+
+            Raises:
+              ValueError: If the provided index is negative
+            """
+            if index < 0:
+                raise ValueError("Index cannot be negative")
+            data_length = len(self.dataset())
+            total_pages = math.ceil(data_length / page_size)
+            if index is None:
+                page_number = 1
+            else:
+                page_number = min(
+                        math.ceil(index / page_size),
+                        total_pages
+                        )
+            start_index = (page_number - 1) * page_size
+            end_index = min(
+                    start_index + page_size,
+                    data_length
+                    )
+            next_index = min(end_index + 1, data_length)
+            assert 0 <= start_index < data_length, "Invalid"
+            paginated_data = self.dataset()[start_index:end_index]
+            return {
+                    "index": start_index,
+                    "next_index": next_index,
+                    "page_size": page_size,
+                    "data": paginated_data,
+                    }
