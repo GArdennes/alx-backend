83c9247721f98039092d488b6eeee26981445a93
GArdennes <thekevin.afachao@gmail.com>
2024-04-04 12:17:34 +0000

    Task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ 2-lifo_cache
 """
+from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -13,6 +14,7 @@ class LIFOCache(BaseCaching):
         """ Initialize
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def print_cache(self):
         """ Print the cache
@@ -26,15 +28,14 @@ class LIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print(f"DISCARD: {last_key}")
         self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            for key, _ in reversed(list(self.cache_data.items())):
-                print(f"DISCARD: {key}")
-                del self.cache_data[key]
-                break
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """ Get an item by key
         """
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
