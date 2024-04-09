2261740c50f17bc42e278a77d3d868b8c9e26e81
GArdennes <thekevin.afachao@gmail.com>
2024-04-09 13:52:13 +0000

    Task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 app: A basic Flask application
 """
@@ -31,7 +32,7 @@ def home() -> str:
     """
     Renders the index.html template for the home page.
     """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
